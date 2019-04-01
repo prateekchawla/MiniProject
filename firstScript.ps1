6cc10246ddbc6674489b49bf3c64c8e6c807b445
Prateek Chawla <prateek.chawla@InspopCorp.com>
2019-04-01 19:52:19 +0530

    commit

---
+++ firstScript.ps1
@@ -0,0 +1,26 @@
+
+
+$azacct = "prateekchawla93@gmail.com"
+$azpassword = "galaxyyoung123"
+$RepoPath = "https://github.com/prateekchawla/SecondGit"
+
+$appID="717152e6-feef-4c22-904e-bad03901940d"
+$password="/DJv.^g!_#Q|)II_[};-1$W/#-$>?r$dI/*&}*6"
+$tenant="a26a5ff8-8542-4fff-b6de-bee30a6b56b7"
+$RESOURCE_NAME="PowerShellWebApp2"
+$PLAN_NAME="Plan2"
+$WEBAPP_NAME="PowerShellWebAppJi2"
+$username = "FirstAutomatedDeployment\$FirstAutomatedDeployment"
+$password = "mmPQX20aJa5LHTmDlLgpJ8NenH4zgkFpryfThookwhEM00ZBB6QgaqSrDN3D"
+
+az login --service-principal -u $appID --password $password --tenant $tenant
+az group create --location westeurope --name $RESOURCE_NAME
+az appservice plan create --name $PLAN_NAME --resource-group $RESOURCE_NAME --sku FREE
+az webapp create --name $WEBAPP_NAME --resource-group $RESOURCE_NAME --plan $PLAN_NAME
+
+
+
+az webapp deployment source config --name $WEBAPP_NAME --resource-group $RESOURCE_NAME --repo-url $RepoPath --branch master --manual-integration
+
+echo http://$WEBAPP_NAME.azurewebsites.net
+
