c22970964a4c30487579196efd7ef484e75582e2
Prateek Chawla <prateekchawla93@gmail.com>
2019-04-02 16:44:32 +0530

    Update firstScript.ps1

--- firstScript.ps1
+++ firstScript.ps1
@@ -1,10 +1,14 @@
 
-
+$azacct = "prateekchawla93@gmail.com"
+$azpassword = "galaxyyoung123"
 $RepoPath = "https://github.com/prateekchawla/SecondGit"
 
 $appID="717152e6-feef-4c22-904e-bad03901940d"
 $password="/DJv.^g!_#Q|)II_[};-1$W/#-$>?r$dI/*&}*6"
 $tenant="a26a5ff8-8542-4fff-b6de-bee30a6b56b7"
+$RESOURCE_NAME="AzureResourceGroup"
+$PLAN_NAME="AzurePlan"
+$WEBAPP_NAME="MyFirstAzureWebsite"
 
 az login --service-principal -u $appID --password $password --tenant $tenant
 az group create --location westeurope --name $RESOURCE_NAME
@@ -12,8 +16,6 @@ az appservice plan create --name $PLAN_NAME --resource-group $RESOURCE_NAME --sk
 az webapp create --name $WEBAPP_NAME --resource-group $RESOURCE_NAME --plan $PLAN_NAME
 
 
-
 az webapp deployment source config --name $WEBAPP_NAME --resource-group $RESOURCE_NAME --repo-url $RepoPath --branch master --manual-integration
 
 echo http://$WEBAPP_NAME.azurewebsites.net
-
