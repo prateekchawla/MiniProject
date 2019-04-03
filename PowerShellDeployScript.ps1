8f48d3c0a2b6a5b8e35ce46c816863239e3b1f8b
Prateek Chawla <prateekchawla93@gmail.com>
2019-04-03 16:55:18 +0530

    Update PowerShellDeployScript.ps1

--- PowerShellDeployScript.ps1
+++ PowerShellDeployScript.ps1
@@ -1,3 +1,3 @@
       send-mailmessage -from "AutomationReportAgent@inspopindia.com" -to "prateek.chawla@elephant.com" -BodyAsHtml -subject "Successful Deployment of" -body "<This is an automated message sent from Jenkins>" -smtpServer EXCHANGE.InspopCorp.com
       
-      send-mailmessage -from "NoReply@Elephant.com" -to $recipients -BodyAsHtml -subject "Successful GuideWire 9 BillingCenter $ENVIRONMENT Deployment" -body "<font face = Calibri>The newest code version has been deployed to the GW9 <b>BillingCenter</b> application in <b>$ENVIRONMENT</b>.<br>The server is now back up and returns the correct page content.<br><br>The log of the code changes included in this deployment is below:<br><font color=#21618C>$GitFileContent</font><br><br><font color=#626567>This is an automated message sent from Jenkins.</font></font>" -smtpServer mailrelay.elephant.com
+   
