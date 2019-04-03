2683abb4851dccdea6956bb520da248e4cdb7c40
Prateek Chawla <prateekchawla93@gmail.com>
2019-04-03 20:29:26 +0530

    Update PowerShellDeployScript.ps1

--- PowerShellDeployScript.ps1
+++ PowerShellDeployScript.ps1
@@ -72,5 +72,6 @@ az webapp deployment source config --name $WEBAPP_NAME --resource-group $RESOURC
 echo $url
 
 choco install curl -yes
+curl -v http://myfirstazurewebsiteadmiral.azurewebsites.net
 $status = curl -Is "http://myfirstazurewebsiteadmiral.azurewebsites.net" | head -n 1
 echo $status
