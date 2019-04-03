44ec851152856abebf2615c66e65409f31555af5
Prateek Chawla <prateekchawla93@gmail.com>
2019-04-03 17:14:41 +0530

    Update PowerShellDeployScript.ps1

--- PowerShellDeployScript.ps1
+++ PowerShellDeployScript.ps1
@@ -1,37 +1,5 @@
-      #Build url to test
-	       $port = "8080"
-	       $url = ""
-	       $recipients = "prateek.chawla@elephant.com"
-	       echo "URL to test : http://myfirstazurewebsiteadmiral.azurewebsites.net" 
-	
-	       $i = 0
-	       $Content_Test = "g"
-	       
-	       $Response = Invoke-WebRequest http://google.com -UseBasicParsing
-	       $Content = $Response.Content 
-	
-	       while(!($Content -match $Content_Test)){
-	              if($i -eq 1){
-	                     #it has been more than 1 minutes and the page never returned good content
-	                     #send out an alert email
-	                     echo "It has been 1 minutes without a good response..."
-	                     echo "Aborting the job and sending an alert email"
-	                     send-mailmessage -from "AutomationReportAgent@inspopindia.com" -to "prateek.chawla@elephant.com" -BodyAsHtml -subject "UnSuccessful Deployment of $WEBAPP_NAME " -body "<This is an automated message sent from Jenkins.</font></font>" -smtpServer EXCHANGE.InspopCorp.com
-	                     exit 1
-	              }
-	              #page is still not returning the correct content
-	              echo "Bad HTTP Content Response"
-	              echo "Sleeping 1 minute..."
-	              Start-Sleep -s 2
-	              $Response = Invoke-WebRequest http://google.com -UseBasicParsing
-	              $Content = $Response.Content
-	              $i++
-	       }
-	
-	       #page has returned the correct content
-	       echo "Good HTTP Content"
-         
-send-mailmessage -from "AutomationReportAgent@inspopindia.com" -to "prateek.chawla@elephant.com" -BodyAsHtml -subject "Successful Deployment of" -body "<This is an automated message sent from Jenkins>" -smtpServer EXCHANGE.InspopCorp.com
- 
-echo http://$WEBAPP_NAME.azurewebsites.net
+
    
+ $recipients = "prateek.chawla@elephant.com"
+   
+          send-mailmessage -from "AutomationReportAgent@inspopindia.com"  -to $recipients -BodyAsHtml -subject "Successful GuideWire 9 BillingCenter $ENVIRONMENT Deployment" -body "<font face = Calibri>The newest code version has been deployed to the GW9 <b>BillingCenter</b> application in <b>$ENVIRONMENT</b>.<br>The server is now back up and returns the correct page content.<br><br>The log of the code changes included in this deployment is below:<br><font color=#21618C>$GitFileContent</font><br><br><font color=#626567>This is an automated message sent from Jenkins.</font></font>" -smtpServer EXCHANGE.InspopCorp.com
