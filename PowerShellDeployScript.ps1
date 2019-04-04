4409f2ea085eee9eee32efde9b757d2c852b0ba9
Prateek Chawla <prateekchawla93@gmail.com>
2019-04-04 17:01:44 +0530

    Update PowerShellDeployScript.ps1

--- PowerShellDeployScript.ps1
+++ PowerShellDeployScript.ps1
@@ -70,12 +70,12 @@ az webapp deployment source config --name $WEBAPP_NAME --resource-group $RESOURC
   
 #Check Http status code of website  
 $HTTP_Status = curl.exe $url -w ", %{http_code}"
-#If ($HTTP_Status -eq 200) {
-#    echo "Site is OK with status code" $HTTP_Status
-#}
-#Else {
-#  echo "The Site may be down, please check!"
-#}
+If ($HTTP_Status -eq 200) {
+   echo "Site is OK with status code" $HTTP_Status
+}
+Else {
+  echo "The Site may be down, please check!"
+}
 
 echo $HTTP_Status
 echo "Your website is:"
